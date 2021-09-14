c5dc3cb7bb3a4c8bb7ac44da6240cc4a2446d9d4
yusinae <82851675+yusinae@users.noreply.github.com>
2021-09-14 09:31:23 +0900

    drawer외부파일처리

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'drawer_contents.dart';
 
 class Home extends StatelessWidget {
 
@@ -47,85 +48,8 @@ class Home extends StatelessWidget {
         child: Text('My Page!'),
       ),
       drawer: Drawer(
-          child: ListView(
-              padding: EdgeInsets.zero,
-              children: <Widget>[
-                UserAccountsDrawerHeader(
-                  currentAccountPicture: FittedBox(
-                    fit: BoxFit.contain,
-                    child: Image.asset('assets/images/sample_coffee.png'),
-                    // child: Text(
-                    //   'Big Sur',
-                    //   style: TextStyle(fontSize: 200, color: Colors.white),
-                    // ),
-                  ),
-                  currentAccountPictureSize: Size(80, 80),
-                  otherAccountsPicturesSize: Size(70, 70),
-                  // currentAccountPicture: FittedBox(
-                  //   child: Image.asset('assets/images/sample_coffee.png'),
-                  //   //fit: BoxFit.fill,
-                  // ),
-                  //currentAccountPicture: new Image.asset('assets/images/qr.jpg', width: 18.0, height: 18.0),
-                  accountName: Text('sinae'),
-                  accountEmail: Text('sinae@test.co.kr'),
-                  onDetailsPressed:(){
-                    print('arrow is clicked');
-                  },
-                  otherAccountsPictures: <Widget>[
-                    Image.asset('assets/images/qr.jpg', width: 28.0, height: 28.0),
-                  ],
-                  decoration: BoxDecoration(
-                    color: Colors.red[200],
-                    borderRadius: BorderRadius.only(
-                      //bottomLeft: Radius.circular(40.0),
-                    ),
-                  ),
-                ),
-                ListTile(
-                  leading: Icon(Icons.home),
-                  title: Text('screenA', style: TextStyle(
-                    fontSize: 15.0,
-                  )),
-                  onTap: (){
-                    Navigator.pop(context);
-                    Navigator.pushNamed(context, '/a');
-                  },
-                  trailing: Icon(Icons.add),
-                  contentPadding: EdgeInsets.symmetric(vertical: 0.0, horizontal:
-                  10.0),
-                  dense:true,
-                ),
-                ListTile(
-                  leading: Icon(Icons.home),
-                  title: Text('screenB', style: TextStyle(
-                    fontSize: 15.0,
-                  )),
-                  onTap: (){
-                    Navigator.pop(context);
-                    Navigator.pushNamed(context, '/b');
-                  },
-                  trailing: Icon(Icons.add),
-                  contentPadding: EdgeInsets.symmetric(vertical: 0.0, horizontal:
-                  10.0),
-                  dense:true,
-                ),
-                ListTile(
-                  leading: Icon(Icons.home),
-                  title: Text('screenC', style: TextStyle(
-                    fontSize: 15.0,
-                  )),
-                  onTap: (){
-                    Navigator.pushNamed(context, '/c');
-                  },
-                  trailing: Icon(Icons.add),
-                  contentPadding: EdgeInsets.symmetric(vertical: 0.0, horizontal:
-                  10.0),
-                  dense:true,
-                ),
-              ]
-          )
-
-      ),
+        child: DrawerContents()
+      )
     );
   }
 }
